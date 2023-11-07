f4614d12859d94766c19363fdd332659e17c22b5
Canario <yeraysantanac@gmail.com>
2023-11-07 18:15:53 +0000

    A18. Comprimir mágenes y generador de CRUD Ejercicio1

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Profile;
+use Intervention\Image\ImageManagerStatic as Image;
 class ProfileController extends Controller
 {
     public function index()
@@ -21,16 +22,31 @@ class ProfileController extends Controller
             // Reemplaza 'imageUpload' por el nombre del campo en tu formulario
         ]);
 
+
+        $requestImage = $request->file('imageUpload');
+        $img = Image::make($requestImage);
+
+        $img->resize(null, 400, function ($constraint) {
+            $constraint->aspectRatio();
+            $constraint->upsize();
+        });
+
+        $name = $requestImage->hashName();
+        $path = 'images/' . $name;
+        $img->save(storage_path('app/public/' . $path));
+        
+        
         if ($request->imageUpload) {
-            $path = $request->file('imageUpload')->store('images', 'public');
-            
+            // $path = $request->file('imageUpload')->store('images', 'public');
+
+
             Profile::updateOrCreate(
                 ['user_id' => Auth::id()],
                 ['imageUpload' => $path]
             );
-            
+
             return back()->with('success', "Your image has been updated.");
-            }
+        }
     }
 
     public function edit(Profile $profile)
